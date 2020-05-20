8bfd278c0e2f8449e748d3d868c220cf6eb7b15b
Amardip Kaur <akaur66@mail.greenriver.edu>
2020-05-20 19:40:42 -0400

    added survey

--- index.php
+++ index.php
@@ -10,10 +10,22 @@ require_once('vendor/autoload.php');
 //instantiate the F3 Base class
 $f3 = Base::instance();
 
-//default route
+//default
 $f3->route('GET /', function(){
     echo '<h1>Midterm Survey</h1>';
     echo '<a href="survey">Take my Midterm Survey</a>';
+});
+
+//survey
+$f3->route('GET|POST /survey', function($f3) {
+
+    $options = array('This midterm is easy', 'I like midterms', 'Today is Monday');
+
+    $f3->set('options', $options);
+
+    $view = new Template();
+    echo $view->render('views/survey.html');
+
 });
 //run fat free
 $f3->run();
