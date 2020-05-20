bd6b80e90af0e4931e0f19c2e85d5b6f33be32a0
Amardip Kaur <akaur66@mail.greenriver.edu>
2020-05-20 19:58:22 -0400

    added summary page

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+//start session
+session_start();
+
 //require autoload file
 require_once('vendor/autoload.php');
 
@@ -20,12 +23,27 @@ $f3->route('GET /', function(){
 $f3->route('GET|POST /survey', function($f3) {
 
     $options = array('This midterm is easy', 'I like midterms', 'Today is Monday');
-
     $f3->set('options', $options);
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        //store data in the session array
+        $_SESSION['name'] = $_POST['name'];
+        $_SESSION['options'] = $_POST['options'];
+
+        //redirect to summary page
+        $f3->reroute('summary');
+    }
 
     $view = new Template();
     echo $view->render('views/survey.html');
 
+});
+
+//summary
+$f3->route('GET /summary', function(){
+    $view = new Template();
+    echo $view->render('views/summary.html');
 });
 //run fat free
 $f3->run();
